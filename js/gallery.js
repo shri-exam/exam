29665a75bfaaf61bd6423b77b26a3d2465cb220f
unl0k <unl0k@mail.ru>
2013-03-29 02:05:41 +0200

    Add preloading to albums

--- js/gallery.js
+++ js/gallery.js
@@ -27,24 +27,40 @@ $(document).ready(function(){
 
     /* Global events */
 
+
     var user = "kuzckin-o";
     
+    // Sprite preloade
+    $('<img>').attr('src','./img/sprites.png');
+
     $.when( APP.modules.YandexPhotoAPI.loadAlbums( user ) ).then(function(albums){
         
+        // setup albums container        
         $('.album-bar__items').width( albums.length * 100 );        
         $('.album-bar__items').css({"margin":"20px auto"});        
-        $('.album-bar__items').hide();  
-
+ 
+        // create view
         viewBarElement = new APP.modules.JStpl('templates.album-bar__items__item-templ');
         
         $.each(albums,function(index,item) {
-
-            viewBarElement.setParam({'imageCount' : item.count, 'src' : item.image});
+            // create preloader animation
+            var preloader = new APP.modules.animation({'speed':50,'frames':12,'imagePath':'./img/sprites.png'});
+            
+            // templating
+            viewBarElement.setParam({'id' : item.id, 'imageCount' : item.count,'src':'./img/clear.png','preloader': preloader.getHTML()});
+            // add template to DOM
             $('.album-bar__items').append( viewBarElement.getHTML() );
-
+            
+            // async image loading
+            $('<img>').attr('src',item.image).load(function(){
+                // hide image
+                $('span[data-id="'+item.id+'"]').parent().find('img').attr('src',item.image).hide();
+                // show image
+                $('img[src="'+item.image+'"]').fadeIn(1000);
+                // remove preloader animation
+                preloader.destroy();
+            });
         });
-
-        $('.album-bar__items').fadeIn(800);
     });
 
 });
